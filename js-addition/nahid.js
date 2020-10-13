d8aa9408fc08084f7b38a31304ff0e3b057e8c67
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2020-10-14 00:49:19 +0600

    javascript-addition

---
+++ js-addition/nahid.js
@@ -0,0 +1,7 @@
+function add(){
+    let a,b,c;
+    a=Number(document.querySelector("#first").value);
+    b=Number(document.querySelector("#second").value);
+    c= a + b;
+    document.querySelector("#answer").value= c;
+    }
