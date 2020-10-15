717809cab159a657de03e7d2f9553ba54caaf1ae
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2020-10-15 23:15:29 +0600

    browser alert

---
+++ alert/main.js
@@ -0,0 +1,5 @@
+window.alert("Hello");
+var regPassword="Nahid123";
+var userInput="Nahid123";
+var result=(regPassword== userInput)? "  You've succesfully logged into the site. ":"  Sorry you've enter the wrong password! ";
+document.querySelector(".demo").innerHTML=result;
